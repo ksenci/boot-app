5fa621384c58392eb5a87269afb55cbbcdb50e18
Ksenija Noll <ksenija.noll@gmail.com>
2018-09-20 22:39:32 +0200

    Typo fix

--- server.js
+++ server.js
@@ -4,8 +4,7 @@
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const
-  request = require('request');
-express = require('express'),
+  express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
 
