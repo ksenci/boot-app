eb1cda04c91d6a4c9b3300d9961f0f0ef0e2b7ce
Ksenija Noll <ksenija.noll@gmail.com>
2018-09-24 18:12:42 +0200

    Added support for apiai.

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 
 // Imports dependencies and set up http server
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const request = require('request');
+const APIAI_TOKEN = process.env.APIAI_TOKEN;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 
 const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+    request = require('request'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    apiai = require('apiai');
+
+const apiaiApp = apiai(APIAI_TOKEN);
+
+
+const app = express();
+app.use(bodyParser.json());// creates express http server
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
@@ -16,38 +24,27 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
 
-  let body = req.body;
+    let body = req.body;
 
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+        body.entry.forEach(function (entry) {
+            let webhook_event = entry.messaging[0];
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
-
-      // Gets the body of the webhook event
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
         handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
-      }
+      } 
     });
 
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.sendStatus(404);
+    }
 
 });
 
@@ -55,78 +52,66 @@ app.post('/webhook', (req, res) => {
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "a4Uac9a2e"
+    let VERIFY_TOKEN = "a4Uac9a2e"
 
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
 
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
     }
-  }
 });
 
 
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
+function handleMessage(event) {
 
 
-  let response;
+    let sender = event.sender.id;
+    let text = event.message.text;
+    let apiai = apiaiApp.textRequest(text, {
+        sessionId: 'tabby_cat'
+      });
 
-  // Check if the message contains text
-  if (received_message.text) {
+      apiai.on('response', (response) => {        
+        callSendAPI(sender, response);          
+      });
+ 
 
-    // Create the payload for a basic text message
-    response = {
-      "text": `Hello! You sent the message: "${received_message.text}"!`
-    }
-  }
+    apiai.on('error', (error) => {
+        console.log(error);
+    });
 
-  // Sends the response message
-  callSendAPI(sender_psid, response);
-
+    apiai.end();
 }
 
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
-  // Send the HTTP request to the Messenger Platform
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
+    // Construct the message body
+    let aiText = response.result.fulfillment.speech;
+ console.log(aiText);
+  
+   
+    // Send the HTTP request to the Messenger Platform
+    request({
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "POST",
+        "json":   {  
+          recipient: {id: sender_psid},
+          message: {text: aiText}
+        }
+    }, (err) => {
+        if (!err) {
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
